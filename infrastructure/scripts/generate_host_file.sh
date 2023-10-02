b2e25306c81091eae00fd1ed25c78528b536b6b3
AnotherUser969 <132815401+AnotherUser969@users.noreply.github.com>
2023-10-02 22:48:59 +1000

    Update generate_host_file.sh

--- infrastructure/scripts/generate_host_file.sh
+++ infrastructure/scripts/generate_host_file.sh
@@ -2,6 +2,6 @@
 #Создание ansible host файла из terraform output
 
 echo [masters] > hosts
-terraform output -json | jq -r '.masters_ips.value.internal[][]' >> hosts
+terraform -chdir="./infrastructure/terraform" output -json | jq -r '.masters_ips.value.internal[][]' >> hosts
 echo [workers] >> hosts
-terraform output -json | jq -r '.workers_ips.value.internal[][]' >> hosts
+terraform -chdir="./infrastructure/terraform" output -json | jq -r '.workers_ips.value.internal[][]' >> hosts
