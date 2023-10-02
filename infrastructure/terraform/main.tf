a5840b5320ad074ba68c29aef69ea27a7fe98425
AnotherUser969 <132815401+AnotherUser969@users.noreply.github.com>
2023-10-02 15:43:13 +1000

    Update main.tf

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -15,28 +15,83 @@ provider "yandex" {
 resource "yandex_compute_instance_group" "master" {
   name                = "test-ig-master"
   folder_id           = "${var.folder_id}"
-  service_account_id  = "aje2qo68d01hqiolpukk"
+  service_account_id  = "${var.service_account_id}"
   deletion_protection = false
   instance_template {
     name = "master-{instance.index}"
-	hostname = "master-{instance.index}"
+    hostname = "master-{instance.index}"
     platform_id = "standard-v3"
     resources {
-      memory = 2
-      cores  = 4
-	  core_fraction = 20
+      memory = 1
+      cores  = 2
+      core_fraction = 20
     }
     boot_disk {
       mode = "READ_WRITE"
       initialize_params {
-	    type = "network-hdd"
+	type = "network-hdd"
         image_id = "fd830gae25ve4glajdsj"
         size     = 8
       }
     }
     network_interface {
       //network_id = "${yandex_vpc_network.network.id}"
-	  network_id = "enp7m02qd9vbo1m3r75q"
+      network_id = "${var.network_id}"
+      subnet_ids = ["${yandex_vpc_subnet.subnet.id}"]
+      dns_record {
+	fqdn = "test-ig."
+       }
+    }
+    metadata = {
+      foo      = "bar"
+      //ssh-keys = "${file("~/.ssh/id_rsa.pub")}"
+      user-data = "${file("~/metadata")}"
+    }
+    network_settings {
+      type = "STANDARD"
+    }
+  }
+  scale_policy {
+    fixed_scale {
+      size = 2
+    }
+  }
+  allocation_policy {
+    zones = ["ru-central1-b"]
+  }
+  deploy_policy {
+    max_unavailable = 2
+    max_creating    = 2
+    max_expansion   = 2
+    max_deleting    = 2
+  }
+}
+
+resource "yandex_compute_instance_group" "worker" {
+  name                = "test-ig-worker"
+  folder_id           = "${var.folder_id}"
+  service_account_id  = "${var.service_account_id}"
+  deletion_protection = false
+  instance_template {
+    name = "worker-{instance.index}"
+    hostname = "worker-{instance.index}"
+    platform_id = "standard-v3"
+    resources {
+      memory = 1
+      cores  = 2
+      core_fraction = 20
+    }
+    boot_disk {
+      mode = "READ_WRITE"
+      initialize_params {
+	type = "network-hdd"
+        image_id = "fd830gae25ve4glajdsj"
+        size     = 8
+      }
+    }
+    network_interface {
+      //network_id = "${yandex_vpc_network.network.id}"
+      network_id = "${var.network_id}"
       subnet_ids = ["${yandex_vpc_subnet.subnet.id}"]
 	  dns_record {
 	     fqdn = "test-ig."
@@ -46,7 +101,7 @@ resource "yandex_compute_instance_group" "master" {
     metadata = {
       foo      = "bar"
       //ssh-keys = "${file("~/.ssh/id_rsa.pub")}"
-	  user-data = "${file("~/metadata")}"
+      user-data = "${file("${var.user_data_path}")}"
     }
     network_settings {
       type = "STANDARD"
@@ -68,65 +123,6 @@ resource "yandex_compute_instance_group" "master" {
     max_creating    = 2
     max_expansion   = 2
     max_deleting    = 2
-  }
-}
-
-resource "yandex_compute_instance_group" "worker" {
-  name                = "test-ig-worker"
-  folder_id           = "b1g3a65ovr0uv7n30pq7"
-  service_account_id  = "aje2qo68d01hqiolpukk"
-  deletion_protection = false
-  instance_template {
-    name = "worker-{instance.index}"
-	hostname = "worker-{instance.index}"
-    platform_id = "standard-v3"
-    resources {
-      memory = 2
-      cores  = 2
-	  core_fraction = 20
-    }
-    boot_disk {
-      mode = "READ_WRITE"
-      initialize_params {
-	    type = "network-hdd"
-        image_id = "fd830gae25ve4glajdsj"
-        size     = 8
-      }
-    }
-    network_interface {
-      //network_id = "${yandex_vpc_network.network.id}"
-      network_id = "enp7m02qd9vbo1m3r75q"
-      subnet_ids = ["${yandex_vpc_subnet.subnet.id}"]
-	  dns_record {
-	     fqdn = "test-ig."
-	  }
-    }
-
-    metadata = {
-      foo      = "bar"
-      //ssh-keys = "${file("~/.ssh/id_rsa.pub")}"
-	  user-data = "${file("~/metadata")}"
-    }
-    network_settings {
-      type = "STANDARD"
-    }
-  }
-
-  scale_policy {
-    fixed_scale {
-      size = 0
-    }
-  }
-
-  allocation_policy {
-    zones = ["ru-central1-b"]
-  }
-
-  deploy_policy {
-    max_unavailable = 1
-    max_creating    = 1
-    max_expansion   = 1
-    max_deleting    = 1
   }
 }
 
