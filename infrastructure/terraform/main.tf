074dc9715cdac5858afcbc92c59bcfaf0380cd4c
AnotherUser969 <132815401+AnotherUser969@users.noreply.github.com>
2023-10-02 15:21:00 +1000

    Update main.tf

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -130,36 +130,6 @@ resource "yandex_compute_instance_group" "worker" {
   }
 }
 
-//resource "yandex_vpc_network" "network" {
-//  name = "yc-auto-network"
-//}
-
-resource "yandex_vpc_subnet" "subnet" {
-  //count          = "${var.cluster_size > length(var.zones) ? length(var.zones) : var.cluster_size}"
-  name           = "yc-auto-subnet"
-  zone           = "ru-central1-b"
-  //network_id     = "${yandex_vpc_network.network.id}"
-  network_id = "enp7m02qd9vbo1m3r75q"
-  v4_cidr_blocks = ["192.168.10.0/24"]
-  route_table_id = yandex_vpc_route_table.rt.id
-}
-
-resource "yandex_vpc_gateway" "nat_gateway" {
-  name = "nat-gateway"
-  shared_egress_gateway {}
-}
-
-resource "yandex_vpc_route_table" "rt" {
-  name       = "route-table"
-  //network_id = "${var.network_id}"
-  network_id = "enp7m02qd9vbo1m3r75q"
-
-  static_route {
-    destination_prefix = "0.0.0.0/0"
-    gateway_id         = yandex_vpc_gateway.nat_gateway.id
-  }
-}
-
 locals {
    masters_ips = {
 	 internal = ["${yandex_compute_instance_group.master.instances.*.network_interface.0.ip_address}"]
@@ -167,15 +137,4 @@ locals {
    workers_ips = {
 	 internal = ["${yandex_compute_instance_group.worker.instances.*.network_interface.0.ip_address}"]
    }
-   prod_subnet_ids = yandex_vpc_subnet.subnet.*.id
-}
-
-output "masters_ips" {
-  value = "${local.masters_ips}"
-}
-output "workers_ips" {
-  value = "${local.workers_ips}"
-}
-output "prod_subnet_ids" {
-  value = "${local.prod_subnet_ids}"
 }
