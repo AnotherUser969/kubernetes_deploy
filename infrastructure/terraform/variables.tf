95e76df7d78b794f893f73240736f22ffb22846c
AnotherUser969 <132815401+AnotherUser969@users.noreply.github.com>
2023-10-02 15:47:53 +1000

    Update variables.tf

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,1 +1,18 @@
-
+variable "user_data_path" {
+	default = "metadata/userdata"
+}
+variable "service_account_key_file" {
+	
+}
+variable "folder_id" {
+	default = "b1g3a65ovr0uv7n30pq7"
+}
+variable "service_account_key_file" {
+	default = "b1g3a65ovr0uv7n30pq7"
+}
+variable "network_id" {
+	
+}
+variable "cird_net_block" {
+	default = 10
+}
