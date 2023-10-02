fbef2144a52c89ded372b79cb9c59ad466c6ba23
AnotherUser969 <132815401+AnotherUser969@users.noreply.github.com>
2023-10-02 21:36:08 +1000

    Update variables.tf

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -2,7 +2,7 @@ variable "user_data_path" {
 	default = "metadata/userdata"
 }
 variable "service_account_key_file" {
-	
+	default = "aje2qo68d01hqiolpukk"
 }
 variable "folder_id" {
 	default = "b1g3a65ovr0uv7n30pq7"
@@ -11,7 +11,7 @@ variable "service_account_key_file" {
 	default = "b1g3a65ovr0uv7n30pq7"
 }
 variable "network_id" {
-	
+	default = "enptqk925duvk7t2q8mp"
 }
 variable "cird_net_block" {
 	default = 10
