c125ad3d342fdb5611b117f7a1a980df4d714034
AnotherUser969 <132815401+AnotherUser969@users.noreply.github.com>
2023-10-02 15:19:20 +1000

    Create output.tf

---
+++ infrastructure/terraform/output.tf
@@ -0,0 +1,9 @@
+output "masters_ips" {
+  value = "${local.masters_ips}"
+}
+output "workers_ips" {
+  value = "${local.workers_ips}"
+}
+output "prod_subnet_ids" {
+  value = "${local.prod_subnet_ids}"
+}
